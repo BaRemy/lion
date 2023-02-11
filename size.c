34c3bfbb21066a2793bc736b31379b3d97758b60
BaRemy <bahatiremy05@gmail.com>
2023-02-11 10:55:24 -0800

    Sizeofop

---
+++ size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program to print the size of an integer
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Value of an int:%d\n", sizeof(int));
+	return (0);
+}
